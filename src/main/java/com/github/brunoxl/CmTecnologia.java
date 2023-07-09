4d459b50b0c1481c90e1a7a8d1b82346d46f10eb
Bruno <brunox.leitao@gmail.com>
2023-07-09 15:30:00 -0300

    feat: imlementacao ex1 e ex2

--- src/main/java/com/github/brunoxl/CmTecnologia.java
+++ src/main/java/com/github/brunoxl/CmTecnologia.java
@@ -1,4 +1,38 @@
 package src.main.java.com.github.brunoxl;
 
+import java.util.Arrays;
+
 public class CmTecnologia {
+
+    public static int[][] invertMatrixDiagonals(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n / 2; i++) {
+            int temp = matrix[i][i];
+            matrix[i][i] = matrix[n - i - 1][n - i - 1];
+            matrix[n - i - 1][n - i - 1] = temp;
+
+            temp = matrix[i][n - i - 1];
+            matrix[i][n - i - 1] = matrix[n - i - 1][i];
+            matrix[n - i - 1][i] = temp;
+        }
+        return matrix;
+    }
+
+    public static int numberOfMatrixInside(int[][] A, int[][] B) {
+        int numberLines_a = A.length;
+        int numberColumns_a = A[0].length;
+        int size_a = numberLines_a * numberColumns_a;
+
+        int numberLines_b = B.length;
+        int numberColumns_b = B[0].length;
+        int size_b = numberLines_b * numberColumns_b;
+
+        if (size_a < size_b) return 0;
+        if (numberLines_a == numberLines_b && numberColumns_a == numberColumns_b) return 1;
+        if (numberLines_a == numberLines_b) return numberColumns_a - numberColumns_b + 1;
+        if (numberColumns_a == numberColumns_b) return numberLines_a - numberLines_b + 1;
+
+        return 0;
+    }
 }
--- src/test/java/com/github/brunoxl/CmTecnologiaTest.java
+++ src/test/java/com/github/brunoxl/CmTecnologiaTest.java
@@ -1,4 +1,111 @@
 package src.test.java.com.github.brunoxl;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import src.main.java.com.github.brunoxl.CmTecnologia;
+
 public class CmTecnologiaTest {
+
+    @Test
+    public void shouldInvert2x2MatrixDiagonals() {
+        int[][] matrix = {{1, 2}, {3, 4}};
+
+        int[][] expectedResult = {{4, 3}, {2, 1}};
+        int[][] resultMatrix = CmTecnologia.invertMatrixDiagonals(matrix);
+
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Assertions.assertEquals(expectedResult[i][j], resultMatrix[i][j]);
+            }
+        }
+    }
+
+    @Test
+    public void shouldInvert3x3MatrixDiagonals() {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        int[][] expectedResult = {{9, 2, 7}, {4, 5, 6}, {3, 8, 1}};
+        int[][] resultMatrix = CmTecnologia.invertMatrixDiagonals(matrix);
+
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Assertions.assertEquals(expectedResult[i][j], resultMatrix[i][j]);
+            }
+        }
+    }
+
+    @Test
+    public void shouldInvert4x4MatrixDiagonals() {
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+
+        int[][] expectedResult = {{16, 2, 3, 13}, {5, 11, 10, 8}, {9, 7, 6, 12}, {4, 14, 15, 1}};
+        int[][] resultMatrix = CmTecnologia.invertMatrixDiagonals(matrix);
+
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Assertions.assertEquals(expectedResult[i][j], resultMatrix[i][j]);
+            }
+        }
+    }
+
+    @Test
+    public void ShouldGetNumberOfSubMatrixWhenBLargerThanA() {
+        int[][] a = {{1, 2}};
+        int[][] b = {{1, 2}, {3, 4}};
+
+        int result = CmTecnologia.numberOfMatrixInside(a, b);
+
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public void ShouldGetNumberOfSubMatrixWhenBSameSizeThanA() {
+        int[][] a = {{1, 3}, {3, 5}};
+        int[][] b = {{1, 2}, {3, 4}};
+
+        int result = CmTecnologia.numberOfMatrixInside(a, b);
+
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    public void ShouldGetNumberOfSubMatrixNumberOfLinesIsEqual() {
+        int[][] a = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}};
+        int[][] b = {{1, 2}, {3, 4}, {5, 6}};
+        int[][] c = {{1, 2, 0}, {3, 4, 0}, {5, 6, 0}};
+        int[][] d = {{1, 2, 0, 0}, {3, 4, 0, 0}, {5, 6, 0, 0}};
+
+        int result;
+
+        result = CmTecnologia.numberOfMatrixInside(a, b);
+        Assertions.assertEquals(4, result);
+
+        result = CmTecnologia.numberOfMatrixInside(a, c);
+        Assertions.assertEquals(3, result);
+
+        result = CmTecnologia.numberOfMatrixInside(a, d);
+        Assertions.assertEquals(2, result);
+    }
+
+    @Test
+    public void ShouldGetNumberOfSubMatrixNumberOfColumnsIsEqual() {
+        int[][] a = {{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}};
+        int[][] b = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+        int[][] c = {{1, 2}, {3, 4}, {5, 6}};
+        int[][] d = {{1, 2}, {3, 4}};
+
+        int result;
+
+        result = CmTecnologia.numberOfMatrixInside(a, b);
+        Assertions.assertEquals(2, result);
+
+        result = CmTecnologia.numberOfMatrixInside(a, c);
+        Assertions.assertEquals(3, result);
+
+        result = CmTecnologia.numberOfMatrixInside(a, d);
+        Assertions.assertEquals(4, result);
+    }
 }
